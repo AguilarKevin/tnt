bb6613dc053dceb239843e01bc674e8678e4a317
aguilarkevin <kevinaguilar0813@gmail.com>
2023-03-29 13:18:23 -0600

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
+const {fontFamily} = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/ui/**/*.{ts,tsx}',
+  ],
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        //your custom colors here
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+      screens: {
+        xs: '480px',
+      },
+    },
+  },
 }
-
